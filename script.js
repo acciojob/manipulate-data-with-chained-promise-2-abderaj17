7951fa533afad7e48158ec292235e3633d8485aa
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-11-13 13:33:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const outputField = document.querySelector("#output");
+
+function PromiseChaining(){
+    let display = new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            let arr = [1,2,3,4];
+            resolve(arr);
+        }, 3000);
+    }).then((numbers)=>{
+        // Filter out the odd numbers
+        let evenNumbers = numbers.filter(num => num % 2 === 0);
+        outputField.textContent = evenNumbers.join(", ");
+        return evenNumbers;
+    }).then((evenNumbers) => {
+        // Multiply the even numbers by 2
+        let multipliedNumbers = evenNumbers.map(num => num * 2);
+        setTimeout(() => {
+            outputField.textContent = multipliedNumbers.join(", ");
+        }, 2000);
+    });
+}
+
+PromiseChaining();
